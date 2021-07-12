84abf966f7fdc85d6930f255e2c20b45495f1496
Jack Haystead <jackhaystead@gmail.com>
2021-07-12 22:13:51 +1200

    feat: added stylelint and prettier for scss linting

---
+++ prettier.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // Options: https://prettier.io/docs/en/options.html
+  endOfLine: 'lf'
+};
---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-sass-guidelines',
+    'stylelint-prettier/recommended'
+  ],
+  rules: {
+    // Rules: https://stylelint.io/user-guide/rules/list
+    'prettier/prettier': true
+  }
+};
