f18daa0d47001877f32254e0833885d6978fcc63
Curtis <curada30@student.wintec.ac.nz>
2022-03-03 12:59:53 +1300

    Added missing translations for it_IT

--- scripts/language-file-validator.js
+++ scripts/language-file-validator.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const masterFilename = 'en_AU.all.json';
 const masterFile = openFile(masterFilename);
 const masterFileKeys = Object.keys(masterFile);
-const languageFilenames = ['hu_HU.all.json'];
+const languageFilenames = ['it_IT.all.json'];
 console.log(languageFilenames);
 const languageFiles = openFiles(languageFilenames);
 
