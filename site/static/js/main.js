e9535f173d1b8cf83cf4c5632096f7668ee57541
Louis Saunders <louis@indiereign.com>
2022-03-16 18:00:01 +1300

    Basic in-page theme editor poc

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -1,5 +1,6 @@
 import './modernizr-custom.js';
 import './can-be-watched-button.component.js';
+import './theme-editor.js';
 
 /*global Swiper, Modernizr, s72*/
 
---
+++ site/static/js/theme-editor.js
@@ -0,0 +1,38 @@
+var primaryColorPicker = document.getElementById('color-picker-primary');
+var secondaryColorPicker = document.getElementById('color-picker-secondary');
+var bodyBgColorPicker = document.getElementById('color-picker-body-bg');
+var bodyColorColorPicker = document.getElementById('color-picker-body-color');
+var logoWidthPicker = document.getElementById('logo-width');
+var headerHeightPicker = document.getElementById('header-height');
+
+function setThemeVar(name, value, unit) {
+  var r = document.querySelector(':root');
+  r.style.setProperty(name, value + (unit || ''));
+}
+
+function hexTorgb(hex) {
+  return ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
+}
+
+primaryColorPicker.addEventListener('input', function (e) {
+  var primaryHex = hexTorgb(e.target.value);
+  setThemeVar('--primary-rgb', primaryHex);
+});
+secondaryColorPicker.addEventListener('input', function (e) {
+  var secondaryHex = hexTorgb(e.target.value);
+  setThemeVar('--secondary-rgb', secondaryHex);
+});
+bodyBgColorPicker.addEventListener('input', function (e) {
+  var bodyBgHex = hexTorgb(e.target.value);
+  setThemeVar('--body-bg-rgb', bodyBgHex);
+});
+bodyColorColorPicker.addEventListener('input', function (e) {
+  var bodyColorHex = hexTorgb(e.target.value);
+  setThemeVar('--body-color-rgb', bodyColorHex);
+});
+logoWidthPicker.addEventListener('input', function (e) {
+  setThemeVar('--navbar-brand-min-width-xl', e.target.value, 'px');
+});
+headerHeightPicker.addEventListener('input', function (e) {
+  setThemeVar('--navbar-brand-padding-y', e.target.value, 'px');
+});
