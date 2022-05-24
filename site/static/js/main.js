f328d20d63649b17ccdd7ff76d5ab68e7e5cf574
Jack Haystead <jack@shift72.com>
2022-05-25 01:53:34 +1200

    it works

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -478,7 +478,7 @@ function documentReady(app) {
     }
   }
 
-  document.querySelectorAll('.btn-trailer').forEach(btn => {
+  document.querySelectorAll('.s72-btn-trailer').forEach(btn => {
     btn.addEventListener('click', e => {
       e.preventDefault();
       e.stopPropagation();
