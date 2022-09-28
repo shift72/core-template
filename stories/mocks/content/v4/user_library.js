aea501caec0c39e33f54002ec0028a459597ee28
Matthew Thomson <matthew.thomson@shift72.com>
2022-09-29 11:12:08 +1300

    film detail page fetch component

---
+++ stories/mocks/content/v4/user_library.js
@@ -0,0 +1,74 @@
+export const currentlyRenting = {
+  url: `${window.location.origin}/services/content/v4/user_library/123/index`,
+  method: 'GET',
+  status: 200,
+  response: [
+    {
+        "item": "/film/123",
+        "info": {
+            "expiry": `${new Date().addHours(24*7).toISOString()}`,
+            "quality": "hd",
+            "available_from": "2020-04-17T07:00:00.000Z",
+            "playback_start": null,
+            "playback_end": null,
+            "rental_playback_duration_minutes": 1440,
+            "film_id": 123
+        },
+        "playback_progress": {
+            "completed": false
+        }
+    }
+  ]
+}
+
+export const startedWatchWindow = {
+  url: `${window.location.origin}/services/content/v4/user_library/123/index`,
+  method: 'GET',
+  status: 200,
+  response: [
+    {
+        "item": "/film/123",
+        "info": {
+            "expiry": `${new Date().addHours(6).toISOString()}`,
+            "quality": "hd",
+            "available_from": "2020-04-17T07:00:00.000Z",
+            "playback_start": null,
+            "playback_end": null,
+            "rental_playback_duration_minutes": 1440,
+            "film_id": 123
+        },
+        "playback_progress": {
+            "completed": false
+        }
+    }
+  ]
+}
+
+export const expiredWatchWindow = {
+  url: `${window.location.origin}/services/content/v4/user_library/123/index`,
+  method: 'GET',
+  status: 200,
+  response: [
+    {
+        "item": "/film/123",
+        "info": {
+            "expiry": `${new Date().addHours(-6).toISOString()}`,
+            "quality": "hd",
+            "available_from": "2020-04-17T07:00:00.000Z",
+            "playback_start": null,
+            "playback_end": null,
+            "rental_playback_duration_minutes": 1440,
+            "film_id": 123
+        },
+        "playback_progress": {
+            "completed": false
+        }
+    }
+  ]
+}
+export const emptyLibrary = {
+  url: `${window.location.origin}/services/content/v4/user_library/123/index`,
+  method: 'GET',
+  status: 200,
+  response: []
+}
---
+++ stories/mocks/geo/v1/location/where_am_i.js
@@ -0,0 +1,6 @@
+export const locationNewZealand = {
+    url: `${window.location.origin}/services/geo/v1/location/where_am_i`,
+    method: 'GET',
+    status: 200,
+    response: {"country_code":"nz","ip_address":"111.111.111.11"}
+}
