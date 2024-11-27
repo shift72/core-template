b4f1f2ed7b004b6c1bcb0f8a058ed5b383412af6
Louis Saunders <louis@indiereign.com>
2024-11-27 16:30:40 +1300

    cleanups

--- site/static/js/carousel-video-mutation-observer.js
+++ site/static/js/carousel-video-mutation-observer.js
@@ -1,14 +1,28 @@
 export default function carouselVideoPauseOnChange() {
+  // load the first video
+  let s = document.querySelector('.s72-carousel-item.current video source');
+  if (s) {
+    s.setAttribute('src', s.parentNode.getAttribute('data-src'));
+    s.parentNode.load();
+  }
+
+  // watch all carousel items for class change
   document.querySelectorAll('.s72-carousel-item').forEach(carousel_item => {
     let callback = (mutationsList, observer) => {
       mutationsList.forEach(mutation => {
         if (mutation.attributeName === 'class') {
           try {
             let video = carousel_item.querySelector('video');
-
+            // get video for carousel item and load / resume if current, else pause
             if (video) {
               if (carousel_item.classList.contains('current')) {
-                video.play();
+                let s = video.querySelector('source');
+                if (s.getAttribute('src')) {
+                  video.play();
+                } else {
+                  s.setAttribute('src', video.getAttribute('data-src'));
+                  video.load();
+                }
               } else {
                 video.pause();
               }
@@ -21,7 +35,6 @@ export default function carouselVideoPauseOnChange() {
     };
     const observer = new MutationObserver(callback);
     observer.observe(carousel_item, {
-      childList: true,
       attributes: true,
     });
   });
