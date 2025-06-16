2b88ab458102515593cbd6d9687a17c00bfcb71b
Sam Douglas <sam@shift72.com>
2025-06-16 17:06:43 +1200

    add basic comments embed in film detail page

---
+++ site/static/js/comments.js
@@ -0,0 +1,7 @@
+import s72 from 's72';
+import {bindEachComponent, attrs} from 's72.ui';
+
+bindEachComponent('comment-section', (e) => {
+  const props = attrs(e);
+  s72.comments.mountCommentsForSlug({slug: props.slug, container: e});
+});
