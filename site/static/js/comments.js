4c0db471ae4bdfa9e7a177ff5d79c68c2f4a81a8
Sam Douglas <sam@shift72.com>
2025-07-02 16:02:14 +1200

    comments: only bind the comment-section component if lib loaded

--- site/static/js/comments.js
+++ site/static/js/comments.js
@@ -1,7 +1,9 @@
 import s72 from 's72';
 import {bindEachComponent, attrs} from 's72.ui';
 
-bindEachComponent('comment-section', (e) => {
-  const props = attrs(e);
-  s72.comments.mountCommentsForSlug({slug: props.slug, container: e});
-});
+if (window.s72.comments) {
+  bindEachComponent('comment-section', (e) => {
+    const props = attrs(e);
+    s72.comments.mountCommentsForSlug({slug: props.slug, container: e});
+  });
+}
