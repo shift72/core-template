ad63bdc18124be553a172c59ed7f42ee819069e2
Sam Douglas <sam@shift72.com>
2025-07-03 17:12:00 +1200

    move comments-section feature check inside bindEachComponent

--- site/static/js/comments.js
+++ site/static/js/comments.js
@@ -1,9 +1,9 @@
 import s72 from 's72';
 import {bindEachComponent, attrs} from 's72.ui';
 
-if (window.s72.comments) {
-  bindEachComponent('comment-section', (e) => {
+bindEachComponent('comment-section', (e) => {
+  if (window.s72.comments) {
     const props = attrs(e);
     s72.comments.mountCommentsForSlug({slug: props.slug, container: e});
-  });
-}
+  }
+});
